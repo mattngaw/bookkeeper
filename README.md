fe5261bca20978b4199d9ecdb36181f8dfee1c60
mattngaw <me@mattngaw.com>
2025-07-11 23:00:05 -0500

    Improve /cost autocomplete; add to README

--- README.md
+++ README.md
@@ -2,16 +2,30 @@
 
 A bookkeeping Discord bot that tracks DKP for NewEra, a Celtic Heroes clan on the Belenus server.
 
-## Credit
+## Features
 
-This bot is built from [kkrypt0nn](https://github.com/kkrypt0nn)'s [Python-Discord-Bot-Template](https://github.com/kkrypt0nn/Python-Discord-Bot-Template).
-Credit for most of the code in this repo goes to them.
+The bot features Discord-style autocomplete for command arguments where applicable.
+
+```text
+Clanny commands:
+/attends                                    # Returns the overall attends leaderboard
+/attends <class>                            # Returns the per-class attends leaderboard
+/attends <toon>                             # Returns the amount of attends a toon has
+/kills <toon>                               # Returns the kill counts for a toon
+/cost <item>                                # Returns the attends cost of an item
+/log <toon> <boss>                          # Records a boss attend for toon
+
+Leader commands:
+/handout <toon> <item> <optional-coupon>    # Deducts attends from toon for the item
+/refund <toon> <item> <half|full>           # Adds attends to toon for the item
+/sub <toon> <amount>                        # Manually deducts attends from toon
+/add <toon> <amount>                        # Manually adds attends to toon
+```
 
 ## How to start
 
 To start the bot you simply need to launch, either your terminal (Linux, Mac & Windows), or your Command Prompt (
-Windows)
-.
+Windows).
 
 Before running the bot you will need to install all the requirements with this command:
 
@@ -27,9 +41,10 @@ python bot.py
 
 > **Note**: You may need to replace `python` with `py`, `python3`, `python3.11`, etc. depending on what Python versions you have installed on the machine.
 
-## Built With
+## Credit
 
-- [Python 3.12.9](https://www.python.org/)
+This bot is built from [kkrypt0nn](https://github.com/kkrypt0nn)'s [Python-Discord-Bot-Template](https://github.com/kkrypt0nn/Python-Discord-Bot-Template).
+Credit for most of the code in this repo goes to them.
 
 ## License
 
--- cogs/newera.py
+++ cogs/newera.py
@@ -1,11 +1,29 @@
 import gspread
-from discord import app_commands, Interaction
+import random
+from discord import app_commands, Interaction, Embed
 from discord.ext import commands
 from discord.ext.commands import Context
+
+LOOTTABLE_URL = "https://docs.google.com/spreadsheets/d/12E81Rvhvy4UfFA4frtyMXLXXbyXyTGaZqdCtTFAY3Bo/edit?usp=sharing"
 
 class NE(commands.Cog, name="newera"):
     def __init__(self, bot) -> None:
         self.bot = bot
+
+    """
+    /loottable
+    """
+    @commands.hybrid_command(
+        name="loottable",
+        description="Returns a link to the loot table sheet.",
+    )
+    async def loottable(self, context: Context) -> None:
+        """
+        :param context: The application command context.
+        """
+        e = Embed()
+        e.description = f"Loot table can be found [**here**]({LOOTTABLE_URL})."
+        await context.send(embed=e)
 
     """
     /cost <item name>
@@ -20,13 +38,17 @@ class NE(commands.Cog, name="newera"):
         :param context: The application command context.
         :param item: The selected item.
         """
+        e = Embed()
         if item not in self.bot.items:
-            await context.send(f"Item '{item}' not found in the list.")
-            return
-        sh = self.bot.loottable.get_worksheet(0)
-        row = sh.find(item).row
-        cost = sh.cell(row, 5).value
-        await context.send(f"The cost of **{item}** is **{cost}**.")
+            e.description = f"**{item}** not found on the [list]({LOOTTABLE_URL}). It either does not exist or it is rolled."
+        else:
+            sh = self.bot.loottable.get_worksheet(0)
+            row = sh.find(item).row
+            cost = sh.cell(row, 5).value
+            pic_url = sh.cell(row, 8).value
+            e.description = f"**{item}** costs **{cost}** attends."
+            e.set_image(url=pic_url)
+        await context.send(embed=e)
     @cost.autocomplete("item")
     async def cost_autocomplete(
         self, interaction: Interaction, current: str
@@ -40,7 +62,8 @@ class NE(commands.Cog, name="newera"):
         """
         choice_items = [app_commands.Choice(name=item, value=item) for item in self.bot.items]
         choices = [choice for choice in choice_items if current.lower() in choice.name.lower()]
-        return [] if len(choices) > 25 else choices
+        # return [] if len(choices) > 25 else choices
+        return random.sample(choices, min(len(choices), 25)) if choices else []
 
 async def setup(bot) -> None:
     await bot.add_cog(NE(bot))
